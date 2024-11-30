7f460cb3b050726890acef0a09dac973d0e9ae49
ItsSony2003 <sonythaiduong2003@gmail.com>
2024-11-30 21:10:27 +0700

    Create Program.cs

---
+++ ShapeOOP/ShapeOOP/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeOOP
+{
+    internal class Program
+    {
+        public class Rectangle
+        {
+            double width, height;
+
+            public Rectangle() { }
+
+            public Rectangle(double width, double height)
+            {
+                this.width = width;
+                this.height = height;
+            }
+
+            public double GetArea() 
+            { 
+                return this.width * this.height;
+            }
+
+            public double GetPerimeter()
+            {
+                return (this.width + this.height) * 2;
+            }
+
+            public string Display()
+            {
+                return "\nYour Rectangle {" + "width = " + width + ", height = " + height + "}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the width: ");
+            double width = Double.Parse(Console.ReadLine());
+            Console.Write("Enter the height: ");
+            double height = Double.Parse(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(width, height);
+
+            Console.WriteLine("" + rectangle.Display());
+            Console.WriteLine("\nPerimeter of the Rectangle: " + rectangle.GetPerimeter());
+            Console.WriteLine("Area of the Rectangle: " + rectangle.GetArea());
+            Console.ReadKey();
+        }
+    }
+}
